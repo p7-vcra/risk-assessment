55a9caa4536790430eccca434ecff60c223cd58f
ViktorPlatz <redviktor10@hotmail.com>
2024-11-27 23:08:25 +0100

    ++

--- src/encounters/vessel_encounter.py
+++ src/encounters/vessel_encounter.py
@@ -16,8 +16,6 @@ logger = setup_logger(__name__)
 DISTANCE_THRESHOLD_IN_KM = int(os.getenv('DISTANCE_THRESHOLD_IN_KM', 1))
 TEMPORAL_THRESHOLD_IN_SECONDS = int(os.getenv('TEMPORAL_THRESHOLD_IN_SECONDS', 30))
 TIME_FOR_BATCHES_IN_S = int(os.getenv('TIME_FOR_BATCHES_IN_S', 12))
-RUN_UNTIL_TIMESTAMP = pd.to_datetime(os.getenv('RUN_UNTIL_TIMESTAMP'))
-RUN_FROM_TIMESTAMP = pd.to_datetime(os.getenv('RUN_FROM_TIMESTAMP'))
 
 
 def vessel_encounters(file_name):
@@ -27,19 +25,17 @@ def vessel_encounters(file_name):
     
     last_logged_hour = None  # Track the last logged hour
     batch_data = []  # Buffer to store data for the batch
-    last_batch_time = RUN_FROM_TIMESTAMP
+    last_batch_time = None
 
     # Stream data asynchronously from CSV
     for data in helper.make_datastream_from_csv(file_name):
         timestamp = data['# Timestamp'].iloc[0]
-        if timestamp < RUN_FROM_TIMESTAMP:
-            continue
-        
+
         # Add data to batch
         batch_data.append(data)
 
         # Check if it's time to process the batch
-        if (timestamp - last_batch_time).total_seconds() >= TIME_FOR_BATCHES_IN_S:
+        if last_batch_time is None or (timestamp - last_batch_time).total_seconds() >= TIME_FOR_BATCHES_IN_S:
             # Concatenate all batch data for processing
             batch_df = pd.concat(batch_data, ignore_index=True)
             batch_df = batch_df.drop_duplicates(subset='MMSI', keep='last', ignore_index=True)
@@ -73,10 +69,6 @@ def vessel_encounters(file_name):
             if last_logged_hour is None or current_hour > last_logged_hour:
                 logger.info(f"Current timestamp: {timestamp}")
                 last_logged_hour = current_hour
-
-        # Exit condition for debugging
-        if timestamp == RUN_UNTIL_TIMESTAMP:
-            break
 
     # Final processing if there is leftover data in the batch
     if batch_data:
