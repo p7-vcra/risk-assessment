a3e247dd665729fca9bc6af29c1678c8955e8998
ViktorPlatz <redviktor10@hotmail.com>
2024-11-27 11:45:43 +0100

    Script longest and shortest pair clustering

--- src/helper_scripts/find_longest_and_shortest_pair_clustering.py
+++ src/helper_scripts/find_longest_and_shortest_pair_clustering.py
@@ -1,29 +1,33 @@
 import pandas as pd
 
-# Define the path to your CSV file
-file_path = 'output/output_20241125-12:38:30_cutdown.csv'
+def find_longest_and_shortest_pair_clustering(save=True):
 
-# Read the CSV file into a pandas DataFrame
-df = pd.read_csv(file_path)
+    # Define the path to your CSV file
+    file_path = 'output/output_20241125-12:38:30_cutdown.csv'
 
-# Display the first few rows to verify the data
-print(df.head())
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path)
 
-# Convert the start_time and end_time columns to datetime objects
-df['start_time'] = pd.to_datetime(df['start_time'])
-df['end_time'] = pd.to_datetime(df['end_time'])
+    # Display the first few rows to verify the data
+    print(df.head())
 
-# Calculate the time duration for each encounter in seconds
-df['duration_seconds'] = (df['end_time'] - df['start_time']).dt.total_seconds()
+    # Convert the start_time and end_time columns to datetime objects
+    df['start_time'] = pd.to_datetime(df['start_time'])
+    df['end_time'] = pd.to_datetime(df['end_time'])
 
-# Convert duration in seconds to HH:MM:SS format
-df['duration'] = pd.to_datetime(df['duration_seconds'], unit='s').dt.strftime('%H:%M:%S')
+    # Calculate the time duration for each encounter in seconds
+    df['duration_seconds'] = (df['end_time'] - df['start_time']).dt.total_seconds()
 
-# Sort by distance and duration
-df = df.sort_values(by=['distance', 'duration_seconds'])
+    # Convert duration in seconds to HH:MM:SS format
+    df['duration'] = pd.to_datetime(df['duration_seconds'], unit='s').dt.strftime('%H:%M:%S')
 
-# Display the updated DataFrame with duration
-print(df[['vessel_1', 'vessel_2', 'start_time', 'end_time', 'duration']].head())
+    # Sort by distance and duration
+    df = df.sort_values(by=['distance', 'duration_seconds'])
 
-# Save the updated DataFrame with the duration column to a new CSV file
-df.to_csv('vessel_encounter_data_with_duration.csv', index=False)
+    # Display the updated DataFrame with duration
+    print(df[['vessel_1', 'vessel_2', 'start_time', 'end_time', 'duration']].head())
+
+    # Save the updated DataFrame with the duration column to a new CSV file
+    if save:
+        df.to_csv('vessel_encounter_data_with_duration.csv', index=False)
+    return df
